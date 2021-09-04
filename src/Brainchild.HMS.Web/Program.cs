65f6fe12c66e13fdff351f940ae2ea39661efe0e
sneha-313 <sneha@brainchildschool.com>
2021-09-04 16:10:58 +0530

    Added a simple orchard core application

---
+++ src/Brainchild.HMS.Web/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace Brainchild.HMS.Web
+{
+    public class Program
+    {
+        public static Task Main(string[] args)
+            => BuildHost(args).RunAsync();
+
+        public static IHost BuildHost(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging => logging.ClearProviders())
+                .UseSerilog((hostingContext, configBuilder) =>
+                    {
+                        configBuilder.ReadFrom.Configuration(hostingContext.Configuration)
+                        .Enrich.FromLogContext();
+                    })
+                .ConfigureWebHostDefaults(webBuilder => webBuilder
+                    .UseStartup<Startup>()
+                ).Build();
+    }
+}
