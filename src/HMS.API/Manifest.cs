fb8da30a849747a7c21bc8de84e01daa600d999b
sneha-313 <sneha@brainchildschool.com>
2021-09-17 16:48:12 +0530

    Added the modular application

---
+++ src/HMS.API/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "HMS.API",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "HMS.API",
+    Category = "HMS.API"
+)]
