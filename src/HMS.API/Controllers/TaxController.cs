61c452ee7ee7e462b19f430f5c14883b32915e21
sneha-313 <sneha@brainchildschool.com>
2021-09-18 09:17:31 +0530

    Added Modular Application HMS.API

---
+++ src/HMS.API/Controllers/TaxController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Brainchild.HMS.Core.Models;
+using Brainchild.HMS.Data.Context;
+
+namespace HMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaxController : ControllerBase
+    {
+        private readonly BrainchildHMSDbContext _context;
+
+        public TaxController(BrainchildHMSDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tax
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tax>>> GetTaxes()
+        {
+            return await _context.Taxes.ToListAsync();
+        }
+
+        // GET: api/Tax/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tax>> GetTax(int id)
+        {
+            var tax = await _context.Taxes.FindAsync(id);
+
+            if (tax == null)
+            {
+                return NotFound();
+            }
+
+            return tax;
+        }
+
+        // PUT: api/Tax/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTax(int id, Tax tax)
+        {
+            if (id != tax.TaxId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tax).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaxExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tax
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Tax>> PostTax(Tax tax)
+        {
+            _context.Taxes.Add(tax);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTax", new { id = tax.TaxId }, tax);
+        }
+
+        // DELETE: api/Tax/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTax(int id)
+        {
+            var tax = await _context.Taxes.FindAsync(id);
+            if (tax == null)
+            {
+                return NotFound();
+            }
+
+            _context.Taxes.Remove(tax);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TaxExists(int id)
+        {
+            return _context.Taxes.Any(e => e.TaxId == id);
+        }
+    }
+}
--- src/HMS.API/Startup.cs
+++ src/HMS.API/Startup.cs
@@ -1,20 +1,49 @@
-using System;
+// Unused usings removed
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using OrchardCore.Modules;
-
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Brainchild.HMS.Core.Models;
+using Brainchild.HMS.Data.Context;
 namespace HMS.API
 {
-    public class Startup : StartupBase
+    public class Startup
     {
-        public override void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
-            
+            Configuration = configuration;
         }
 
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddDbContext<BrainchildHMSDbContext>(opt =>
+                                               opt.UseInMemoryDatabase("BrainchildNewHMS"));
+           
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+               
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
