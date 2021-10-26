d5ab2c5eba056655d24e2c3ad439a943da5cd667
sneha-313 <sneha@brainchildschool.com>
2021-10-26 13:14:10 +0530

    Added Payments

---
+++ src/Brainchild.HMS.API/Controllers/PaymentsController.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Brainchild.HMS.Core.Models;
+using Brainchild.HMS.Data.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using System.Collections;
+using Brainchild.HMS.Data;
+using Brainchild.HMS.Data.DTOs;
+
+namespace Brainchild.HMS.API.Controllers
+{
+    [Route("hms/api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = "Api"), IgnoreAntiforgeryToken, AllowAnonymous]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly BrainchildHMSDbContext _context;
+        private readonly ILogger<PaymentsController> _logger;
+        public IPaymentService _paymentService = new PaymentService("Data Source=SNEHA;Initial Catalog=BrainchildHMS;Integrated Security=True;");
+        public PaymentsController(BrainchildHMSDbContext context, ILogger<PaymentsController> logger)
+        {
+            _context = context;
+             _logger = logger;
+        }
+
+        // GET: api/Payments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Payment>>> GetPayments()
+        {
+            return await _context.Payments.ToListAsync();
+        }
+
+        // GET: api/Payments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Payment>> GetPayment(int id)
+        {
+            var payment = await _context.Payments.FindAsync(id);
+
+            if (payment == null)
+            {
+                return NotFound();
+            }
+
+            return payment;
+        }
+
+        // PUT: api/Payments/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPayment(int id, Payment payment)
+        {
+            if (id != payment.PaymentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(payment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Payments
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Payment>> PostPayment(PaymentDTO payment)
+        {
+            try
+            {
+                //Creating Payments
+                _logger.LogInformation($"_paymentService.AddPayments Method called with parameters {payment.PaymentTypeId}, {payment.PaymentAmount},{payment.PaymentDate},{payment.PaymentDescription} and {payment.BillingId}");
+                _paymentService.AddPayments(payment);
+                _logger.LogInformation("Payments Added Successfully.");
+                
+                return CreatedAtAction("GetPayment", new { id = payment.PaymentId }, payment);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError($"Exception: {exception}");
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // DELETE: api/Payments/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePayment(int id)
+        {
+            var payment = await _context.Payments.FindAsync(id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Payments.Remove(payment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PaymentExists(int id)
+        {
+            return _context.Payments.Any(e => e.PaymentId == id);
+        }
+    }
+}
---
+++ src/Brainchild.HMS.Data/PaymentService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Brainchild.HMS.Data.DTOs;
+using Brainchild.HMS.Core.Models;
+
+namespace Brainchild.HMS.Data
+{
+
+    public interface IPaymentService
+    {
+        void AddPayments(PaymentDTO payment);
+    }
+    public class PaymentService : IPaymentService
+    {
+        private readonly string connectionString;
+        public PaymentService(string connection)
+        {
+            connectionString = connection;
+        }
+        public void AddPayments(PaymentDTO payment)
+        {
+            //creating an sqlconnection object.
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            //opening the connection.
+            sqlConnection.Open();
+            //SQL query for inserting the payments
+            SqlCommand sqlCommand = new SqlCommand("INSERT INTO Payments VALUES(@paymentTypeId,@paymentAmount,@paymentDescription,@billingId,@paymentDate);SELECT SCOPE_IDENTITY()", sqlConnection);
+            //Adding parameters
+            sqlCommand.Parameters.AddWithValue("@paymentTypeId",payment.PaymentTypeId);
+            sqlCommand.Parameters.AddWithValue("@paymentAmount", payment.PaymentAmount);
+            sqlCommand.Parameters.AddWithValue("@paymentDescription", payment.PaymentDescription);
+            sqlCommand.Parameters.AddWithValue("@billingId", payment.BillingId);
+            sqlCommand.Parameters.AddWithValue("@paymentDate", payment.PaymentDate);
+            //Executing the query and storing the paymentId 
+            payment.PaymentId=Convert.ToInt32(sqlCommand.ExecuteScalar());
+            //Closing the connection
+            sqlConnection.Close();
+
+        }
+    }
+}
