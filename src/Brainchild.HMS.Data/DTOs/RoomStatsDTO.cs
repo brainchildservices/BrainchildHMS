f332e742b61a0b8acf2f6d6db6412cbef983108f
sneha-313 <sneha@brainchildschool.com>
2021-10-29 21:42:06 +0530

    Created DTO and Service class

---
+++ src/Brainchild.HMS.Data/DTOs/RoomStatsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brainchild.HMS.Data.DTOs
+{
+    public class RoomStatsDTO
+    {
+        public DateTime fromDate { get; set; }
+        public DateTime toDate { get; set; }
+    }
+}
---
+++ src/Brainchild.HMS.Data/HotelService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Brainchild.HMS.Data.DTOs;
+using Brainchild.HMS.Core.Models;
+
+namespace Brainchild.HMS.Data
+{
+    public interface IHotelService
+    {
+        
+    }
+    public class HotelService : IHotelService
+    {
+        private readonly string connectionString;
+        public HotelService(string connection)
+        {
+            connectionString = connection;
+        }
+    }
+}
